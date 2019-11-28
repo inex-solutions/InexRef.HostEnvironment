f4bb8073afc0e66f5c1d84efbe183709b100e6c0
Rob Levine <rob@roblevine.co.uk>
2019-11-28 09:01:09 +0000

    can't remember what this is

---
+++ src/InexRef.HostEnvironment.Tests.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using InexRef.HostEnvironment.Hosting;
+using InexRef.HostEnvironment.TestEnvironment.NUnit.Tests.Greeting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InexRef.HostEnvironment.Tests.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var flavour = HostedEnvironment.FlavoursConfiguration.DefaultFlavour;
+
+            var serviceCollection = new ServiceCollection();
+            flavour.ConfigureContainer(serviceCollection);
+
+            var container = serviceCollection.BuildServiceProvider();
+
+            var greeter = container.GetService<IGreeting>();
+
+            Console.WriteLine(greeter.Greet());
+        }
+    }
+}
